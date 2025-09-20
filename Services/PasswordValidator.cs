c2343433a593b0056ec118df37af62b7fe4c9686
FelipeCostaq <felipecostasiqu@gmail.com>
2025-09-20 15:10:09 -0300

    feat: PasswordValidation class and function

--- Services/PasswordValidator.cs
+++ Services/PasswordValidator.cs
@@ -2,5 +2,16 @@
 {
     public class PasswordValidator
     {
+        public static bool StrongPassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+                return false;
+
+            if (password.Length < 8)
+                return false;
+
+            var regex = new System.Text.RegularExpressions.Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$");
+            return regex.IsMatch(password);
+        }
     }
 }
