925302bb5c9e3675763e0cd69df9f5331367ff9a
FelipeCostaq <felipecostasiqu@gmail.com>
2025-09-20 15:16:54 -0300

    feat: UsernameValidator class and function

---
+++ Services/UsernameValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace AuthFinance.Services
+{
+    public class UsernameValidator
+    {
+        public static bool ValidUsername(string username)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                return false;
+
+            if (username.Length < 3)
+                return false;
+
+            var regex = new System.Text.RegularExpressions.Regex(@"^[a-zA-Z0-9]+$");
+            return regex.IsMatch(username);
+        }
+    }
+}
